43998a5476f8581110c7f17c4c7f0018c32f9034
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2024-01-29 14:20:00 +0300

    Update example5.sh

--- example5.sh
+++ example5.sh
@@ -4,14 +4,16 @@
 password="qwerty"
 # Пароль введенный пользователем
 user_input=""
+# Считывает пароль введенный пользователем
+read -p "Enter the password: " user_input
 
 # Цикл выполняется пока не подойдет пароль
 while [ "$user_input" != "$password" ]
 do
-# Считывает пароль введенный пользователем
-  read -p "Enter the password: " user_input
 # Выводит, что пароль неверный
   echo "Wrong password, login denied"
+# Считывает пароль введенный пользователем
+  read -p "Enter the password: " user_input
 done
 
 # Выводит, что пароль верен
